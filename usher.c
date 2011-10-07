60f8f2e3d3eb91a31aa5ecff33dce345db999fa0
James Daniels <james@marginleft.com>
2011-10-07 11:25:12 -0400

    Playing around with C again, this is fun

--- usher.c
+++ usher.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-int main() {
-	puts("Hello world.");
+int main(int argc, char *argv[]) {
 
-	int age = 10;
-	int height;
+	int distance = 100; 
+	float power = 2.345f;
+	double super_power = 56789.4532; 
+	char initial = 'A';
+	char first_name[] = "Zed"; 
+	char last_name[] = "Shaw";
+	int areas[] = {10, 12, 13, 14, 20};
 
-	printf("I am %d years old.\n"); 
-	printf("I am %d inches tall.\n", height);
+	printf("You are %d miles away.\n", distance); 
+	printf("You have %f levels of power.\n", power); 
+	printf("You have %f awesome super powers.\n", super_power); 
+	printf("I have an initial %c.\n", initial); 
+	printf("I have a first name %s.\n", first_name); 
+	printf("I have a last name %s.\n", last_name); 
+	printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
+
+	printf("The size of an int: %ld\n", sizeof(int)); 
+	printf("The size of areas (int[]): %ld\n", sizeof(areas)); 
+	printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int)); 
+	printf("The first area is %d, then 2nd %d.\n", areas[0], areas[1]);
 
 	return 0;
 }
