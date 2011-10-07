3aa0b51e882bc2ee04c80698580cdcaf0965fe4f
James Daniels <james@marginleft.com>
2011-10-07 11:16:38 -0400

    Added a ignore file, some starting c code, a makefile, and some jazz

---
+++ makefile
@@ -0,0 +1,5 @@
+CFLAGS=-Wall -g
+
+clean:
+	rm -f usher
+	rm -rf usher.dSYM
---
+++ usher.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	puts("Hello world.");
+
+	int age = 10;
+	int height;
+
+	printf("I am %d years old.\n"); 
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
